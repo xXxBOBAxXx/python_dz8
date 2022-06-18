3eded1738dae798f9706920d71e5893f7576b311
xXxBOBAxXx <100014195+xXxBOBAxXx@users.noreply.github.com>
2022-06-19 01:35:55 +0300

    add cancel

--- select_task.py
+++ select_task.py
@@ -3,7 +3,8 @@ from print_tasks import print_select
 def to_select(t, find_id):
     for id in find_id:
         print_select(t,id)
-    id = input('выберете номер нужной задачи, любой другой символ - отмена: ')
-    if id in str(find_id):
-        return int(id)
-    else: return
+    id = int(input('выберете номер нужной задачи, любой другой символ - отмена: '))
+    if id in find_id:
+        if type(id) == int:
+            return int(id)
+    else: return None
