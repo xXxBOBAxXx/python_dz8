79c17999aedea1ccdd6ed5a80413227b11f89f7c
xXxBOBAxXx <100014195+xXxBOBAxXx@users.noreply.github.com>
2022-06-19 01:34:26 +0300

    add cancel

--- interface.py
+++ interface.py
@@ -4,7 +4,6 @@ from new_task import to_tasks
 from find_task import to_find
 from select_task import to_select
 from del_task import to_del
-from for_complete_task import to_complete
 
 def run_program():
     to_end = False
@@ -13,9 +12,11 @@ def run_program():
         if command == '1': print_all(t)
         elif command == '2':
             find_id = to_find(t)
-            if len(find_id) > 1: id = to_select(t, find_id)
-            elif len(find_id) == 1: id = find_id
-            else: run_program()
+            if find_id == None: run_program()
+            if len(find_id) > 1: 
+                id = to_select(t, find_id)
+                if id == None: run_program()
+            if len(find_id) == 1: id = find_id
             command = input('выберете команду: 1 - отметить задачу выполненной, 2 - удалить задачу, или другие символы - для отмены: ')
             if command == '1' or '2': to_del(t, id, command)
             else: run_program()
