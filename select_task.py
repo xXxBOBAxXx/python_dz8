2ecd5278b485dc06c6cd924d4a2953913620384c
xXxBOBAxXx <100014195+xXxBOBAxXx@users.noreply.github.com>
2022-06-22 19:52:05 +0300

    Update select_task.py

--- select_task.py
+++ select_task.py
@@ -3,8 +3,8 @@ from print_tasks import print_select
 def to_select(t, find_id):
     for id in find_id:
         print_select(t,id)
-    id = int(input('выберете номер нужной задачи, любой другой символ - отмена: '))
-    if id in find_id:
-        if type(id) == int:
-            return int(id)
+    id = input('выберете номер нужной задачи, любой другой символ - отмена: ')
+    if type(id) != int: return None
+    if id in find_id: return id
     else: return None
+    
