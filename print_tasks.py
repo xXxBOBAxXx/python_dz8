cafa720078954326bbd261d8980c7609ba6cc9b4
xXxBOBAxXx <100014195+xXxBOBAxXx@users.noreply.github.com>
2022-06-22 19:51:37 +0300

    Update print_tasks.py

--- print_tasks.py
+++ print_tasks.py
@@ -3,4 +3,9 @@ def print_all(t):
         print(i[0], i[1])
 
 def print_select(t,id):
-    print(id, '<-',t[id][0], '-', t[id][1])
+    print(id, '<-',t[int(id)][0], '-', t[int(id)][1])
+
+def print_task(t,id):
+    for i in id:
+        print(t[i][0], '-', t[i][1])
+        
