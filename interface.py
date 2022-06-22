0a59855e9bd0c10cda0cdb0714ea82f0dfba5d99
xXxBOBAxXx <100014195+xXxBOBAxXx@users.noreply.github.com>
2022-06-22 19:40:25 +0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,5 +1,6 @@
 from from_tasks import t
 from print_tasks import print_all
+from print_tasks import print_task
 from new_task import to_tasks
 from find_task import to_find
 from select_task import to_select
@@ -15,17 +16,22 @@ def run_program():
         if command == '2':
             find_id = to_find(t)
             if find_id == None: continue
-            if len(find_id) > 1: 
+            elif len(find_id) > 1: 
                 id = to_select(t, find_id)
                 if id == None: continue
-            if len(find_id) == 1: id = find_id
+            elif len(find_id) == 1: 
+                id = find_id
+                print_task(t,id)
             command = input('выберете команду: 1 - отметить задачу выполненной, 2 - удалить задачу, или другие символы - для отмены: ')
-            if command == '1' or '2': to_del(t, id, command)
-            if command == '1': print('задача отмеченна выполненой')
-            if command == '2': print('задача удалена')
-            continue
+            if command == '1' or '2': 
+                if command == '': continue
+                to_del(t, id, command)
+                if command == '1': print('задача отмеченна выполненой')
+                if command == '2': print('задача удалена')
+            else: continue
         if command == '3': 
             to_tasks()
             print('добавлена новая задача')
             continue
         else: to_end = True
+            
