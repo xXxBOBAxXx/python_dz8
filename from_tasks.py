b9b6240671e76a24a73328f31794c3ec2b17f1ad
xXxBOBAxXx <100014195+xXxBOBAxXx@users.noreply.github.com>
2022-06-22 19:50:01 +0300

    Update from_tasks.py

--- from_tasks.py
+++ from_tasks.py
@@ -1,6 +1,9 @@
 import csv
-t = []
-with open("tasks.csv", encoding = 'utf-8') as file:
-    reader = csv.reader(file, delimiter = "-")
-    for i in reader:
-        t.append(i)
+
+def read_tasks():
+    t = []
+    with open("tasks.csv", encoding = 'utf-8') as file:
+        reader = csv.reader(file, delimiter = "-")
+        for i in reader:
+            t.append(i)
+        return t
