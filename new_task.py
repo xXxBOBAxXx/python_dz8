d133813b4a5507d3a96c51e9d36b546a1a65da4e
xXxBOBAxXx <100014195+xXxBOBAxXx@users.noreply.github.com>
2022-06-22 19:50:43 +0300

    Update new_task.py

--- new_task.py
+++ new_task.py
@@ -1,6 +1,7 @@
 import csv
+
 def to_tasks():
-    with open("tasks.csv", mode = "a", newline = '') as file:
+    with open("tasks.csv", mode = "a", newline = '', encoding='utf-8') as file:
         file_writer = csv.writer(file, delimiter = "-")
         t = ['', ''] 
         t[0] = input('Введите описание задачи: ') 
